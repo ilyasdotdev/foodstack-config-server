5b56a8f187104f8b0af7a2f8121244cb7c271c69
Muhammad Ilyas <m.ilyas@live.com>
2025-09-04 00:16:09 +0500

    init

---
+++ src/test/java/io/github/ilyasdotdev/foodstack/configserver/ConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ilyasdotdev.foodstack.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
